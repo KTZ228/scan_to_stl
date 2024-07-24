21afebace7a0db340c8c976b467688dba940259e
Kenneth van der Zee <51954604+KTZ228@users.noreply.github.com>
2024-07-24 17:33:12 +0200

    Switched stl export tool again. close to final

--- scan_to_stl_script.m
+++ scan_to_stl_script.m
@@ -3,9 +3,12 @@ clear, clc, close all
 %% Load data and turn neural tissue into logical
 addpath(genpath('ExportVoxelData'))
 
-subject_name = 'ernie';
+subject_name = 'kenneth';
+filename = sprintf('data_raw/final_tissues_%s.nii.gz', subject_name);
 
-segmented_data = niftiread(sprintf('data_raw/final_tissues_%s.nii.gz', subject_name));
+segmented_data = niftiread(filename);
+segmented_data_info = niftiinfo(filename);
+disp(segmented_data_info.PixelDimensions);
 
 segmented_data(segmented_data==2) = 1;
 segmented_data(segmented_data~=1) = 0;
@@ -13,19 +16,19 @@ segmented_data(segmented_data~=1) = 0;
 segmented_data_logical = logical(segmented_data);
 
 %% Rotate if necessary
-segmented_data_logical = permute(segmented_data_logical, [3,1,2]);
+%segmented_data_logical = permute(segmented_data_logical, [3,1,2]);
 
 %% Limit array size
 % Find the bounds
 [xIndices, yIndices, zIndices] = ind2sub(size(segmented_data_logical), find(segmented_data_logical));
 
 skull_buffer = 4;
-xMin = min(xIndices) - skull_buffer;
-xMax = max(xIndices) + skull_buffer;
-yMin = min(yIndices) - skull_buffer;
-yMax = max(yIndices) + skull_buffer;
-zMin = min(zIndices) + 75;
-zMax = max(zIndices) + skull_buffer;
+xMin = min(xIndices);% - skull_buffer;
+xMax = max(xIndices);% - skull_buffer;
+yMin = min(yIndices);% - skull_buffer;
+yMax = max(yIndices);% + skull_buffer;
+zMin = min(zIndices) + 70;
+zMax = max(zIndices);% + skull_buffer;
 
 % Crop the array
 segmented_data_logical = segmented_data_logical(xMin:xMax, yMin:yMax, zMin:zMax);
@@ -65,11 +68,14 @@ for z = 1:z_dim
     pause(0); % Pause to visualize each layer
 end
 
+segmented_data_size_buffer = size(segmented_data_logical) + 10;
+
 %% Export to mat file
 save(sprintf('segmented_data_%s.mat', subject_name), 'segmented_data_logical')
 
 %% Export to STL
-smoothing_parameters = struct('mode',1, 'itt',10, 'lambda',0.1, 'sigma',0.1);
+smoothing_parameters = struct('mode',1, 'itt',100, 'lambda',0.06, 'sigma',1);
 
-ExportVoxelData(segmented_data_logical,... 'smoothing', smoothing_parameters,...
-    'mesh_name', sprintf('processed_brain_%s', subject_name), 'output_dir', 'data_processed', 'pov', false);
+ExportVoxelData(segmented_data_logical, 'method', 'geometric', 'smoothing', smoothing_parameters,...
+    'mesh_name', sprintf('processed_brain_%s', subject_name), 'output_dir', 'data_processed', ...
+    'img_dim', segmented_data_size_buffer, 'pov', false);
