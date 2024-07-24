5a309003f799472c7b3daec61a39973585f91d08
Kenneth van der Zee <51954604+KTZ228@users.noreply.github.com>
2024-07-24 18:04:17 +0200

    Added documentation and moved to be edited functions to the top

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # scan_to_stl
-Simple script and dependancies to translate anatomical scans to STL files for a 3D printer
+Simple script and dependencies to translate anatomical scans to STL files for a 3D printer.
+
+
+## How to use
+1. Use Charm (used by SimNIBS) to segment your T1 and T2 structural scans into a nifti file with tissue labels.
+2. Ensure that the neural tissues have the labels 1 and 2.
+3. Place the 'final_tissues.nii.gz' file in the data_raw folder and add your subject name to the file. 
+	- `final_tissues_ernie.nii.gz` for example.
+4. Before running the entire script, run the individual sections first to check:
+	- If the brain is the right way up. If not, enable `rotate_brain` and change the order of the x, y and z axes in `rotation_order`.
+	- Whether the cerebellum touches the base. Try to ensure that the cerebellum just touches the top of the stand by adjusting `suspension_height`.
+5. One final thing is that in the translation to STL, it is assumed that the logical voxels are all 1mm<sup>3</sup>. The actual voxel sizes are denoted in the filename.
--- scan_to_stl_script.m
+++ scan_to_stl_script.m
@@ -1,5 +1,14 @@
 clear, clc, close all
 
+%% The following variables can be enabled or adjusted to ensure the model uses
+% a brain that is straight up and is not suspended by a really long brainstem.
+rotate_brain = false;
+rotation_order = [3,1,2]; % notes the new order of the x, y and z axes
+
+% Determine how many layers are removed at the bottom
+% Try to ensure that the cerebellum just touches the stand
+suspension_height = 70;
+
 %% Load data and turn neural tissue into logical
 addpath(genpath('ExportVoxelData'))
 
@@ -16,19 +25,21 @@ segmented_data(segmented_data~=1) = 0;
 segmented_data_logical = logical(segmented_data);
 
 %% Rotate if necessary
-%segmented_data_logical = permute(segmented_data_logical, [3,1,2]);
+if rotate_brain == true
+    segmented_data_logical = permute(segmented_data_logical, rotation_order);
+end
 
 %% Limit array size
 % Find the bounds
 [xIndices, yIndices, zIndices] = ind2sub(size(segmented_data_logical), find(segmented_data_logical));
 
 skull_buffer = 4;
-xMin = min(xIndices);% - skull_buffer;
-xMax = max(xIndices);% - skull_buffer;
-yMin = min(yIndices);% - skull_buffer;
-yMax = max(yIndices);% + skull_buffer;
-zMin = min(zIndices) + 70;
-zMax = max(zIndices);% + skull_buffer;
+xMin = min(xIndices);
+xMax = max(xIndices);
+yMin = min(yIndices);
+yMax = max(yIndices);
+zMin = min(zIndices) + suspension_height;
+zMax = max(zIndices);
 
 % Crop the array
 segmented_data_logical = segmented_data_logical(xMin:xMax, yMin:yMax, zMin:zMax);
@@ -71,11 +82,20 @@ end
 segmented_data_size_buffer = size(segmented_data_logical) + 10;
 
 %% Export to mat file
-save(sprintf('segmented_data_%s.mat', subject_name), 'segmented_data_logical')
+%save(sprintf('segmented_data_%s.mat', subject_name), 'segmented_data_logical')
 
 %% Export to STL
+if rotate_brain == true
+    voxel_size_list = segmented_data_info.PixelDimensions(rotation_order);
+    output_name = sprintf('processed_brain_%s_x%.4f_y%.4f_z%.4f', subject_name, voxel_size_list(1), voxel_size_list(2), voxel_size_list(3));
+else
+    voxel_size_list = segmented_data_info.PixelDimensions;
+    output_name = sprintf('processed_brain_%s_x%.4f_y%.4f_z%.4f', subject_name, voxel_size_list(1), voxel_size_list(2), voxel_size_list(3));
+end
+
+
 smoothing_parameters = struct('mode',1, 'itt',100, 'lambda',0.06, 'sigma',1);
 
 ExportVoxelData(segmented_data_logical, 'method', 'geometric', 'smoothing', smoothing_parameters,...
-    'mesh_name', sprintf('processed_brain_%s', subject_name), 'output_dir', 'data_processed', ...
+    'mesh_name', output_name, 'output_dir', 'data_processed', ...
     'img_dim', segmented_data_size_buffer, 'pov', false);
